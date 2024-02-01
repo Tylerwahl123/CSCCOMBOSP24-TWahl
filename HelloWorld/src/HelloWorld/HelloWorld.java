0cca631bf0ec8b7456527b192f83ad41dd97432b
Wahl <S03060132@ccc.ccofc.edu>
2024-02-01 10:07:45 -0700

    Hello World

---
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello!\nHow are you doing?\n");
+		
+		// TODO Auto-generated method stub
+		System.out.println ("Hello World");
+		System.out.print("Hello");
+		System.out.print("World");
+
+	}
+
+}
