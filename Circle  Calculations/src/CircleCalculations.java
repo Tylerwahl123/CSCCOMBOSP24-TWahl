1dda38630d41b0809d06c657eab42667f6511230
Wahl <S03060132@ccc.ccofc.edu>
2024-02-16 10:13:03 -0700

    2/15

---
+++ Circle  Calculations/src/CircleCalculations.java
@@ -0,0 +1,10 @@
+
+public class CircleCalculations {
+	public static void main(String[] args) {
+	
+System.out.println("Enter radius of circle");
+	
+
+	}
+
+}
--- Grade Calculator/src/GradeCalculator.java
+++ Grade Calculator/src/GradeCalculator.java
@@ -16,20 +16,39 @@ public class GradeCalculator {
 	public static void main(String[] args) {
 			//switch statement or if statement
 		System.out.println("Enter Numeric Grade");
-		Scanner scr = new Scanner(System.in);
-		String grade;
-		
+		Scanner in = new Scanner(System.in);
+		String grade = "";
 		int g = 0;
+		g = in.nextInt();
+		System.out.println("g is equal to " + g );
 		if (g >= 92) {
 			grade = "A";
 		} else if (g >= 89 && g < 92) {
 			grade = "A-";
 		} else if (g >= 87 && g < 89) {
-			
-		}
+			grade = "B+";
+		} else if (g >= 82 && g < 87) {
+			grade = "B"; 
+		} else if (g >= 79 && g < 82) {
+			grade = "B-";
+		} else if (g >= 77 && g < 79) {
+			grade = "C+";
+		} else if (g >= 72 && g < 77) {
+			grade = "C";
+		} else if (g >= 69 && g < 72) {
+			grade = "C-";
+		} else if (g >= 67 && g < 72) {
+			grade = "D+";
+		} else if (g >= 60 && g < 67) {
+			grade = "D";
+		} else if (g >= 0 && g < 60) {
+			grade = "F";
+		}	
+		System.out.println("Your letter grade is..." + grade);
+	}
+}
 		 
 		
 
 
-	}
-}
+
