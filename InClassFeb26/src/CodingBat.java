d6b04907cd633535d20d75942cc52ab2dc405a95
Wahl <S03060132@ccc.ccofc.edu>
2024-03-04 10:12:58 -0700

    3/4

---
+++ InClassFeb26/src/CodingBat.java
@@ -0,0 +1,21 @@
+
+public class CodingBat {
+
+	public static void main(String[] args) {
+
+	}
+
+	public String theEnd(String str, boolean front) {
+		String str = input.next();
+		boolean frontTrue = true;
+		boolean frontFalse = false;
+		System.out.println("Enter string" + (frontTrue || frontFalse));
+	
+		
+			
+		
+		
+	
+		  
+	}
+}
---
+++ InClassFeb26/src/codingbat2.java
@@ -0,0 +1,14 @@
+
+public class codingbat2 {
+
+	public String nonStart(String a, String b) {
+		String a = new input.next();
+		System.out.println("Enter string a ");
+		String b = new input.next();
+		System.out.println("Enter string b");
+		int i = str.substring();
+		while (i < text.length()) {
+			return b.substring(1) + a.substring(1);
+		}
+	}
+}
